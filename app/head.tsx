68db9cdd01f7081f6ceafedab92527b9253209a4
Alvaro Leal <pursuance@gmail.com>
2022-11-24 12:00:34 +0100

    Start page

--- app/head.tsx
+++ app/head.tsx
@@ -4,11 +4,8 @@ export default function Head() {
   return (
     <>
       <DefaultTags />
-      <title>Next.js App Directory Playground</title>
-      <meta
-        name="description"
-        content="A playground to explore new Next.js 13 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching."
-      />
+      <title>Fire Water Tribe</title>
+      <meta name="description" content="A Soul, funk and blues band" />
     </>
   );
 }
